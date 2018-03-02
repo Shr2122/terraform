f5556a9945deff5a6343c2284e17fb910692da8c
Sandeep Rawat <sandeep@opstree.com>
2018-03-02 20:00:55 +0530

    removed local subnet with publically available subnet module

--- staging.tf
+++ staging.tf
@@ -17,43 +17,17 @@ resource "aws_vpc" "main" {
 }
 
 module "pub_sn_a" {
-  source  = "./subnet"
-  vpc_id  = "${aws_vpc.main.id}"
-  sn_cidr = "192.168.0.0/23"
-  sn_name = "PUB_SN_A"
+  source                  = "github.com/opstree-terraform/subnet"
+  vpc_id                  = "${aws_vpc.main.id}"
+  cidr                    = "192.168.0.0/23"
+  az                      = "us-west-2a"
+  map_public_ip_on_launch = "true"
 }
 
 module "pub_sn_b" {
-  source  = "./subnet"
-  vpc_id  = "${aws_vpc.main.id}"
-  sn_cidr = "192.168.2.0/23"
-  sn_name = "PUB_SN_B"
-}
-
-module "pvt_sn_app_a" {
-  source  = "./subnet"
-  vpc_id  = "${aws_vpc.main.id}"
-  sn_cidr = "192.168.4.0/23"
-  sn_name = "PVT_SN_APP_A"
-}
-
-module "pvt_sn_app_b" {
-  source  = "./subnet"
-  vpc_id  = "${aws_vpc.main.id}"
-  sn_cidr = "192.168.6.0/23"
-  sn_name = "PVT_SN_APP_B"
-}
-
-module "pvt_sn_db_a" {
-  source  = "./subnet"
-  vpc_id  = "${aws_vpc.main.id}"
-  sn_cidr = "192.168.8.0/23"
-  sn_name = "PVT_SN_DB_A"
-}
-
-module "pvt_sn_db_b" {
-  source  = "./subnet"
-  vpc_id  = "${aws_vpc.main.id}"
-  sn_cidr = "192.168.10.0/23"
-  sn_name = "PVT_SN_DB_B"
+  source                  = "github.com/opstree-terraform/subnet"
+  vpc_id                  = "${aws_vpc.main.id}"
+  cidr                    = "192.168.2.0/23"
+  az                      = "us-west-2b"
+  map_public_ip_on_launch = "true"
 }
--- subnet/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_subnet" "subnet" {
-  vpc_id     = "${var.vpc_id}"
-  cidr_block = "${var.sn_cidr}"
-
-  tags {
-    Name = "${var.sn_name}"
-  }
-}
