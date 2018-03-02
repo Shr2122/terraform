6ca0979f8cecea715eb67843febc1e740519f690
Sandeep Rawat <sandeep@opstree.com>
2018-03-02 19:25:55 +0530

    Removed hardcoded values from variable names

--- staging.tf
+++ staging.tf
@@ -1,7 +1,17 @@
+variable "vpc_name" {
+  default     = "Staging"
+  description = "Name tag for the VPC to be created"
+}
+
+variable "vpc_cidr" {
+  default     = "192.168.0.0/20"
+  description = "Name tag for the VPC to be created"
+}
+
 resource "aws_vpc" "main" {
-  cidr_block = "192.168.0.0/20"
+  cidr_block = "${var.vpc_cidr}"
 
   tags {
-    Name = "Staging"
+    Name = "${var.vpc_name}"
   }
 }
